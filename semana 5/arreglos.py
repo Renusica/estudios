69397fde6000af43ec82b7e2412ccc6e549906e2
Halam Hidalgo <halamjhidalgotorres@gmail.com>
2025-06-19 14:04:55 -0400

    contunuacion de los estudios

--- semana 5/arreglos.py
+++ semana 5/arreglos.py
@@ -4,6 +4,8 @@ caitlyns = ["muere","vive","mata","vuelvo con vi"]
 #seleccion = int(input("elije un numero del 1 al 4: "))
 #seleccion = caitlyn
 
-for caitlyn in caitlyns:
-    print(f"Caitlyn: {caitlyn}")
+#for caitlyn in caitlyns:
+#    print(f"Caitlyn: {caitlyn}")
 
+for indece,caitlyn in enumerate (caitlyns):
+    print(f"Caitlyn: {caitlyn} en {indece + 1}")
